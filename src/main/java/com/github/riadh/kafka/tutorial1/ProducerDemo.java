67b4d2f2d1fc4e2ae228ec82ded40d3a8e416fea
Riadh BEL HADJ SALAH <riadh_bel_hadj2004@hotmail.com>
2019-03-03 18:35:13 +0300

    first commit

---
+++ src/main/java/com/github/riadh/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,9 @@
+package com.github.riadh.kafka.tutorial1;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Al salam 3alaikom");
+    }
+
+}
